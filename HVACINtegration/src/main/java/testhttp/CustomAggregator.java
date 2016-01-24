2fef06c4415a83cf7e1a4340e15b7d70281fb4f7
durga <durgaprasadkumar@gmail.com>
2016-01-24 14:59:00 -0800

    Sample MULE project

---
+++ HVACINtegration/src/main/java/testhttp/CustomAggregator.java
@@ -0,0 +1,16 @@
+package testhttp;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.routing.AggregationContext;
+import org.mule.routing.AggregationStrategy;
+
+public class CustomAggregator implements AggregationStrategy {
+
+	@Override
+	public MuleEvent aggregate(AggregationContext context) throws MuleException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ HVACINtegration/src/main/java/testhttp/TagDetails.java
@@ -0,0 +1,9 @@
+package testhttp;
+
+import java.io.Serializable;
+
+public class TagDetails  implements Serializable{
+	public String tagName ;
+	public String buildingId ;
+	public String zipCode ;
+}
